e13f41cf03764d1a5f17c0526d4fa18f76d782e1
dongjin-ina <djshin319@gmail.com>
2025-07-16 20:32:40 +0900

    feat: Subject[]에서 mom이 없을 경우 mom을 -1로 변경

--- src/utils/SubjTester.ts
+++ src/utils/SubjTester.ts
@@ -1,7 +1,15 @@
 import type { Subject } from "../types/SubjType";
 
-export const test_SubjState_idx = (pInfos: Subject[]) => {
+export const test_idx_duplicate = (pInfos: Subject[]) => {
   return pInfos.filter((pInfo, index) => {
     return index === pInfos.findIndex((qInfo) => qInfo.idx === pInfo.idx);
+  });
+};
+
+export const test_mom_empty = (pInfos: Subject[]) => {
+  const pIdxs = new Set(pInfos.map((pInfo) => pInfo.idx));
+  return pInfos.map((pInfo) => {
+    if (pInfo.mom >= 0 && pIdxs.has(pInfo.mom)) return pInfo;
+    return { ...pInfo, mom: -1 };
   });
 };
