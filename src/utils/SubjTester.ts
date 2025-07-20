449ef083849517fc2d744f6612d4e4ddc3ac3bf6
dongjin-ina <djshin319@gmail.com>
2025-07-20 13:02:17 +0900

    feat: test for mom

--- src/utils/SubjTester.ts
+++ src/utils/SubjTester.ts
@@ -1,19 +1,12 @@
 import type { Subject } from "../types/SubjType";
 
-export const testForIdx = (pInfos: Subject[]) => {
-  return pInfos.filter((pInfo, index) => {
-    return index === pInfos.findIndex((qInfo) => qInfo.idx === pInfo.idx);
-  });
-};
-
-export const test_mom_empty = (pInfos: Subject[]) => {
-  const pIdxs = new Set(pInfos.map((info) => info.idx));
-  return pInfos.map((info) => {
-    const [i, m] = [info.idx, info.mom];
-    if (m >= 0 && m !== i && pIdxs.has(m)) return info;
-    return { ...info, mom: -1 };
-  });
-};
+export const testForIdx = (pInfos: Subject[]) =>
+  pInfos
+    .filter(
+      (pInfo, index) =>
+        index === pInfos.findIndex((qInfo) => qInfo.idx === pInfo.idx)
+    )
+    .sort((a, b) => a.idx - b.idx);
 
 export const testForMom = (pInfos: Subject[]) => {
   let restIdxs = new Set(pInfos.map((info) => info.idx));
