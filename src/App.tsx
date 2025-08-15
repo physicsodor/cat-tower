827607edb3e0cc4d9f45b40c62401952c0d52a33
physicsodor <physicsodor@naver.com>
2025-08-15 18:29:41 +0900

    feat: add delSbj and slc.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { DefSbj, type Subject } from "./types/Subject";
 
 function App() {
   const [sbjList, setSbjList] = useState<Subject[]>([]);
+  const [slcSet, setSlcSet] = useState<Set<number>>(new Set());
 
   const addSbj = () => {
     const idxList = sbjList.map((sbj) => sbj.idx).sort((a, b) => a - b);
@@ -12,11 +13,18 @@ function App() {
       if (i !== idxList[i]) break;
     }
     setSbjList((pSbjList) => [...pSbjList, DefSbj(i)]);
+    setSlcSet(new Set([i]));
+  };
+
+  const delSbj = () => {
+    setSbjList((pSbjList) => pSbjList.filter((sbj) => !slcSet.has(sbj.idx)));
+    setSlcSet(new Set());
   };
 
   return (
     <div className="App">
       <button onClick={addSbj}>추가</button>
+      <button onClick={delSbj}>제거</button>
       <div>{JSON.stringify(sbjList)}</div>
     </div>
   );
